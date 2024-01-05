9196c89c3f02ff615de03241272fc31a087befb0
Travis <ondegotravis@gmail.com>
2024-01-05 10:59:51 +0300

    Insertion sort worked out

---
+++ 2-selection_sort.c
@@ -0,0 +1,11 @@
+#include "sort.h"
+/**
+ * selection_sort - sorts a doubly linked list of integers in ascending order
+ * @array: pointer to pointer to head of list to sort
+ * @size: 
+*/
+void selection_sort(int *array, size_t size)
+{
+    
+
+}
