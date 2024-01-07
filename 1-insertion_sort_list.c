5f187dd5edbd9fe2d5c368e7725c624804a75e27
otienojoseph <otienoj.cruz@gmail.com>
2024-01-07 23:36:31 +0300

    fix: betty issues

--- 1-insertion_sort_list.c
+++ 1-insertion_sort_list.c
@@ -1,45 +1,45 @@
 #include "sort.h"
 /**
- * insertion_sort_list - sorts a doubly linked list of integers in ascending order
+ * insertion_sort_list - sorts a doubly linked list
+ * of integers in ascending order
  * @list: pointer to pointer to head of list to sort
+ * Return: void
 */
 void insertion_sort_list(listint_t **list)
 {
-    listint_t *current, *tmp;
+	listint_t *current, *tmp;
 
-    current = *list;
+	current = *list;
 
-    while(current)
-    {
-        /*prev = (current)->prev;*/
-        while(current->next != NULL && (current->n > current->next->n))
-        /**head - prev - current - next - tail*//*tmp*/
-        /**
-         * current - previous - next
-        */
-        {
-            tmp = current->next;
-            /*head - prev - current - tmp ... next - tail*/
-            current->next = tmp->next;
-            /*head - prev - current - tmp - next - tail*/
-            tmp->prev = current->prev;
-            /*head - prev .. tmp - current - next - tail*/
-            if (current->prev)
-                current->prev->next = tmp;
-                /*head - prev - tmp ... current - next - tail*/
-            if (tmp->next)
-                tmp->next->prev = current;
-                /*head - prev - temp - curent - next - tail*/
-            
-            current->prev = tmp;
-            tmp->next = current;
-            if (tmp->prev)
-                current = tmp->prev;
-            else
-                *list = tmp;
-            print_list(*list);
-        }
-        current = current->next;
-  }
+	while (current)
+	{
+		while (current->next != NULL && (current->n > current->next->n))
+		/**head - prev - current - next - tail*//*tmp*/
+		/**
+		 * current - previous - next
+		*/
+		{
+			tmp = current->next;
+			/*head - prev - current - tmp ... next - tail*/
+			current->next = tmp->next;
+			/*head - prev - current - tmp - next - tail*/
+			tmp->prev = current->prev;
+			/*head - prev .. tmp - current - next - tail*/
+			if (current->prev)
+				current->prev->next = tmp;
+				/*head - prev - tmp ... current - next - tail*/
+			if (tmp->next)
+				tmp->next->prev = current;
+				/*head - prev - temp - curent - next - tail*/
 
+			current->prev = tmp;
+			tmp->next = current;
+			if (tmp->prev)
+				current = tmp->prev;
+			else
+				*list = tmp;
+			print_list(*list);
+		}
+		current = current->next;
+	}
 }
