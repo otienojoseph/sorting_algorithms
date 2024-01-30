faf6d3205842847cd0343107e4e434dbb411fd9d
otienojoseph <otienoj.cruz@gmail.com>
2024-01-30 23:10:28 +0300

    fix: Add betty compliance

--- 2-selection_sort.c
+++ 2-selection_sort.c
@@ -2,22 +2,23 @@
 
 /**
  * selection_sort - sorts a doubly linked list of integers in ascending order
- * @arrayay: pointer to pointer to head of list to sort
- * @size: 
+ * @array: pointer to pointer to head of list to sort
+ * @size: length of the array
+ * Return: void
 */
 void selection_sort(int *array, size_t size)
 {
-    int min_index, temp;
-    size_t i, j;
+	int min_index, temp;
+	size_t i, j;
 
 	/* Traverse through all array elements */
 	for (i = 0; i < size - 1; i++)
-    {
+	{
 		/* Find the minimum element in the unsorted part of the array */
 		min_index = i;
 
 		for (j = i + 1; j < size; j++)
-        {
+		{
 			if (array[j] < array[min_index])
 				min_index = j;
 		}
@@ -27,6 +28,6 @@ void selection_sort(int *array, size_t size)
 		array[i] = array[min_index];
 
 		array[min_index] = temp;
-        print_array(array, size);
+		print_array(array, size);
 	}
 }
